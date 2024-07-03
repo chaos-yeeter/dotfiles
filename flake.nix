17aa178b15fa040bde31868e9c8dad2fb47b36d0
chaos-yeeter <kayasthamihir11@gmail.com>
2024-07-03 18:58:19 +0530

    feat(flake): Add pinned nixpkgs input
    - Pin input to a3c8d64ba846725f040582b2d3b875466d2115bd

--- flake.nix
+++ flake.nix
@@ -1,19 +1,30 @@
 {
-  description = "my cool flake!";
+    description = "my cool flake!";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
-  };
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-24.05";
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      lib = nixpkgs.lib;
-    in {
-    nixosConfigurations = {
-      frankenstein = lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
-      };
+        # ref: https://nixos-and-flakes.thiscute.world/nixos-with-flakes/downgrade-or-upgrade-packages
+        nixpkgs-a3c8d6.url = "github:nixos/nixpkgs/a3c8d64ba846725f040582b2d3b875466d2115bd";
     };
-  };
+
+    outputs = { self, nixpkgs, nixpkgs-a3c8d6, ... }:
+    let
+        lib = nixpkgs.lib;
+        system = "x86_64-linux";
+    in {
+        nixosConfigurations = {
+            frankenstein = lib.nixosSystem {
+                inherit system;
+                specialArgs = {
+                    pkgs-a3c8d6 = import nixpkgs-a3c8d6 {
+                        inherit system;
+                        config.allowUnfree = true;
+                    };
+                };
+
+                modules = [ ./configuration.nix ];
+            };
+        };
+    };
 }
