bf37c55fdce6d6608b18e871e534b5e3280c874d
chaos-yeeter <kayasthamihir11@gmail.com>
2025-03-15 12:03:11 +0530

    feat(hyprland): Change to a random wallpaper every 30 minutes
    - Move wallpapers to ~/Pictures/wallpapers/

---
+++ dotfiles/.config/scripts/periodic_random_wallpaper.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# wait till hyprpaper starts
+while (hyprctl hyprpaper listloaded 2>&1 | grep -q "^Couldn't connect to"); do
+    echo "waiting for hyprpaper to start"
+    sleep 1
+done
+
+WALLPAPER_DIR="$HOME/Pictures/wallpapers/"
+
+while true; do
+    current_wallpaper=$(hyprctl hyprpaper listloaded)
+    # get random wallpaper & set it
+    new_wallpaper=$(find -L "$WALLPAPER_DIR" -type f | grep -v "$current_wallpaper" | shuf -n 1)
+    if [ -z "$new_wallpaper" ]; then
+        new_wallpaper="$current_wallpaper"
+    fi
+    hyprctl hyprpaper reload ,"$new_wallpaper"
+    hyprctl hyprpaper unload all
+    sleep 30m
+done
