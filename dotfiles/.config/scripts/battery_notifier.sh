d6762a07a3effe317b3a65336b6a381551a394e8
chaos-yeeter <kayasthamihir11@gmail.com>
2025-04-23 22:28:56 +0530

    fix(notifications): Stop frequent alerts for battery level
    - When battery level is <=15 (discharging) or =100 (charging),
      notification would play every second
    - This happens because we are replacing the notification rather than
      sending new one
    - Fix this by sending new notification rather than replacing existing
      one

--- dotfiles/.config/scripts/battery_notifier.sh
+++ dotfiles/.config/scripts/battery_notifier.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 
-notification_id=12345
 battery_device="$(find /sys/class/power_supply/ -maxdepth 1 -name "*BAT*" | tail -n 1)"
+notification_id=0
 while true; do
     battery_level="$(cat ${battery_device}/capacity)"
     charging_status="$(cat ${battery_device}/status)"
 
-    if [ "$charging_status" == "Discharging" ]; then
-        if [ "$battery_level" -le 15 ]; then
-            dunstify --urgency "critical" --replace "$notification_id" "Battery low" "Level: ${battery_level}%";
-        else
-            dunstify --close "$notification_id";
-        fi
+    if [ "$charging_status" == "Discharging" ] && [ "$battery_level" -le 15 ]; then
+        notification_id=$(dunstify --urgency "critical" "Battery low" "Level: ${battery_level}%" --printid);
+    elif [ "$charging_status" == "Charging" ] && [ "$battery_level" -eq 100 ]; then
+        notification_id=$(dunstify --urgency "critical" "Battery full" --printid);
     else
-        if [ "$battery_level" -eq 100 ]; then
-            dunstify --urgency "critical" --replace "$notification_id" "Battery full";
-        else
-            dunstify --close "$notification_id";
-        fi
+        dunstify --close "$notification_id";
     fi
 
     sleep 1;
