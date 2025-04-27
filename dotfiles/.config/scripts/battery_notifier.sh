ed8c8a0d730ee23c768ed64ebe9a405a478ba36e
chaos-yeeter <kayasthamihir11@gmail.com>
2025-04-27 10:10:57 +0530

    fix(hyprland)!: Handle 'Not charging' for battery notifications
    - Battery status is 'Not charging' when charger is plugged in but power is
      not going to battery
    - Use 'Discharging' behaviour to handle this case
    - Remove battery level from notification body
    
    BREAKING: Battery level will no longer be available in `Battery low`
    notifications

--- dotfiles/.config/scripts/battery_notifier.sh
+++ dotfiles/.config/scripts/battery_notifier.sh
@@ -1,18 +1,38 @@
 #!/usr/bin/env bash
 
+shopt -s extglob
+
 battery_device="$(find /sys/class/power_supply/ -maxdepth 1 -name "*BAT*" | tail -n 1)"
 notification_id=0
+notification_active=0
 while true; do
     battery_level="$(cat ${battery_device}/capacity)"
     charging_status="$(cat ${battery_device}/status)"
 
-    if [ "$charging_status" == "Discharging" ] && [ "$battery_level" -le 15 ]; then
-        notification_id=$(dunstify --urgency "critical" "Battery low" "Level: ${battery_level}%" --printid);
-    elif [ "$charging_status" == "Charging" ] && [ "$battery_level" -eq 100 ]; then
-        notification_id=$(dunstify --urgency "critical" "Battery full" --printid);
-    else
-        dunstify --close "$notification_id";
-    fi
+    case "$battery_level:$charging_status" in
+
+        # battery full
+        '100:Charging')
+            if [ "$notification_active" -eq 0 ]; then
+                notification_id=$(dunstify --urgency "critical" "Battery full" --printid);
+                notification_active=1;
+            fi
+            ;;
+
+        # <0 to 15>:<Discharging/Not charging>
+        @([0-9]|1[0-5]):@(Discharging|Not\ charging))
+            if [ "$notification_active" -eq 0 ]; then
+                notification_id=$(dunstify --urgency "critical" "Battery low" --printid);
+                notification_active=1;
+            fi
+            ;;
+
+        *)
+            [ "$notification_id" -ne 0 ] && dunstify --close "$notification_id";
+            notification_active=0;
+            ;;
+
+    esac
 
     sleep 1;
 done
