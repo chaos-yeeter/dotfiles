de2a7ee25d8fc5f1a04db3bd2477fe0217295e61
chaos-yeeter <kayasthamihir11@gmail.com>
2025-07-14 18:00:10 +0530

    feat(neovim)!: Simplify config
    - Ctrl+[ is now the escape key
    - Set options directly in init.lua. Keep most of the old options
        - Enable persistent undos
        - Keep 10 lines around cursor when scrolling
    - Set keymaps directly in init.lua
        - Map `p` to paste in visual mode while retaining copied text
        - Change horizontal & vertical split keymaps to Ctrl+h & Ctrl+v
        - Add descriptions for all keymaps
    
    BREAKING: All plugins are disabled. Package manager has been removed.
    Escape is now Ctrl+[.

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -1,27 +1,66 @@
--- load custom configs
-require("utils")
-require("editor")
-require("useful-maps")
+-- highlight current line & enable relative line numbers
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 
--- lazy.nvim setup
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- enable smart case for search & stop highlighting matches
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
+-- disable swap file & enable persistent undo
+vim.opt.swapfile = false
+vim.opt.undofile = true
 
-vim.opt.rtp:prepend(lazypath)
+-- hide mode indicator & disable mouse
+vim.opt.showmode = false
+vim.opt.mouse = ""
 
--- load files from lua/plugins
-require("lazy").setup("plugins", {
-	change_detection = {
-		notify = false,
-	},
-})
+-- set tab width
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+
+-- enable sign column & add guide after 100th column
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "101"
+
+-- keep minimum 10 lines around cursor when scrolling
+vim.opt.scrolloff = 10
+
+-- disable conceal
+vim.opt.conceallevel = 0
+
+-- set command line height to 1
+vim.opt.cmdheight = 1
+
+-- buffer & window management
+vim.keymap.set("n", "<leader>w", ":wa<CR>", { desc = "write all changed buffers" })
+vim.keymap.set("n", "<leader>c", ":silent bp | sp | bn | bd!<CR>", { desc = "close current buffer" })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "quit current window" })
+vim.keymap.set("n", "<leader>Q", ":qa<CR>", { desc = "quit neovim" })
+
+-- window navigation
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { desc = "go to window to the left of current window" })
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { desc = "go to window below the current window" })
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { desc = "go to window above the current window" })
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { desc = "go to window to the right of current window" })
+
+-- split management
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.keymap.set("n", "<C-w>v", ":vsplit<CR>", { desc = "create vertical split" })
+vim.keymap.set("n", "<C-w>h", ":split<CR>", { desc = "create horizontal split" })
+
+-- page scroll & search result navigation
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "center half page down scroll" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "center half page up scroll" })
+vim.keymap.set("n", "n", "nzz", { desc = "center search result when going forward" })
+vim.keymap.set("n", "p", "pzz", { desc = "center search result when going backward" })
+
+-- system clipboard integration
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "copy to system clipboard" })
+
+-- miscellaneous
+vim.keymap.set("x", "p", '"_dP', { desc = "retain text when pasting over selection" })
+vim.keymap.set("n", "<leader>zz", ":tabnew %<CR><C-o>zz", { desc = "open current file in new tab" })
