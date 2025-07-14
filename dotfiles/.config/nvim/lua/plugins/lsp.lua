d7bdee6c981b7cb38a31851d4fc8fa65e470ffb5
chaos-yeeter <kayasthamihir11@gmail.com>
2025-07-14 18:42:44 +0530

    ref(neovim): Merge snippets & LSP into single file

--- dotfiles/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -5,5 +5,13 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-buffer",
+
+		-- snippet support
+		{
+			"L3MON4D3/LuaSnip",
+			version = "v2.*",
+			build = "make install_jsregexp",
+		},
+		"saadparwaiz1/cmp_luasnip",
 	},
 }
