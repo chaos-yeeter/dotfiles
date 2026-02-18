7532e6657781b303a58115899d2c065f572fe5b8
chaos-yeeter <kayasthamihir11@gmail.com>
2026-02-18 19:26:44 +0530

    fix(neovim,treesitter): Switch to master branch to fix breaking changes
    - Latest treesitter has breaking changes
    - Because of general lack of documentation (& stability), we want to
      postpone upgrade to latest version

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	-- FIXME: we have pinned to master branch to avoid upgrading to latest `main` branch
+	--        upgrade to main when it's stable
+	branch = "master",
 	build = ":TSUpdate",
 }
